e728b4a7ab761ddb19f8eecf05288ec59e4f741d
Patrícia Castro <patricia.castro@fabamaq.com>
2019-12-16 14:19:51 +0000

    Add importing npm modules challenge

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,17 +1,15 @@
-// const add = require('./utils.js')
-// const sum = add(4, -2)
-// console.log(sum)
-
-//
-// Challenge: Define and use a function in a new file
-//
-// 1. Create a new file called notes.js
-// 2. Create getNotes function that returns "Your notes..."
-// 3. Export getNotes function
-// 4. From app.js, load in and call the function printing message to console
-
+const chalk = require('chalk')
 const getNotes = require('./notes.js')
 
 const notes = getNotes()
-
 console.log(notes)
+
+console.log(chalk.green.bold.inverse('Success!'))
+
+// Challenge: Use chalk library in your project
+// 1. Install version 2.4.1 of chalk
+// 2. Load chalk into app.js
+// 3. Use it to print the string "Success!" to the console in green
+// 4. Test your work
+//
+// Bonus: Use docs to mess around with other styles. Make text bold and inversed.
