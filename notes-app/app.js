b0fdf242f0c9e7a9763d36eeecc061aaf2f50e96
Patrícia Castro <patricia.castro@fabamaq.com>
2019-12-16 12:23:48 +0000

    Add Challenge 2 and add getNotes method

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,12 +1,17 @@
-const fs = require('fs')
+// const add = require('./utils.js')
+// const sum = add(4, -2)
+// console.log(sum)
 
-// fs.writeFileSync('notes.txt', 'My name is Pat.')
-
-
-// Challenge: Append a message to notes.txt
 //
-// 1. Use appendFileSync to append to the file
-// 2. Run the script
-// 3. Check your work by opening the file and viewing the appended text
+// Challenge: Define and use a function in a new file
+//
+// 1. Create a new file called notes.js
+// 2. Create getNotes function that returns "Your notes..."
+// 3. Export getNotes function
+// 4. From app.js, load in and call the function printing message to console
 
-fs.appendFileSync('notes.txt', ' I am 27 years old and I live in Gondomar.')
+const getNotes = require('./notes.js')
+
+const notes = getNotes()
+
+console.log(notes)
