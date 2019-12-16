20eefd01986b7b87ccbba1c1fd68cb154a4b101f
Patrícia Castro <patricia.castro@fabamaq.com>
2019-12-16 15:28:09 +0000

    Add Yargs commands first challenge

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,15 +1,49 @@
 const chalk = require('chalk')
+const yargs = require('yargs')
 const getNotes = require('./notes.js')
 
-const notes = getNotes()
-console.log(notes)
+// Customize yargs version
+yargs.version('1.1.0')
 
-console.log(chalk.green.bold.inverse('Success!'))
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function() {
+        console.log('Adding a new note...')
+    }
+})
 
-// Challenge: Use chalk library in your project
-// 1. Install version 2.4.1 of chalk
-// 2. Load chalk into app.js
-// 3. Use it to print the string "Success!" to the console in green
-// 4. Test your work
-//
-// Bonus: Use docs to mess around with other styles. Make text bold and inversed.
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function() {
+        console.log('Removing the note...')
+    }
+})
+
+// Challenge: Add two new commands
+// 1. Setup command to support read command (placeholder message)
+// 2. Setup command to support list command (placeholder message)
+// 3. Test your work by running both commands and ensure correct output
+
+// Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function() {
+        console.log('Reading the note...')
+    }
+})
+
+//Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List the notes',
+    handler: function() {
+        console.log('Listing the notes...')
+    }
+})
+
+console.log(yargs.argv)
