45243acb44cc1886e6b3a08c9fd1e07b2f3cb46b
Patrícia Castro <patricia.castro@fabamaq.com>
2019-12-16 16:38:45 +0000

    Add JSON and filesystem challenge

---
+++ playground/1-json.js
@@ -0,0 +1,32 @@
+const fs = require('fs')
+// const book = {
+//     title: 'Ego is the Enemy',
+//     author: 'Ryan Holiday'
+// }
+// const dataBuffer = fs.readFileSync('1-json.json')
+// const dataJSON = dataBuffer.toString()
+// const data = JSON.parse(dataJSON)
+
+// const bookJSON = JSON.stringify(book)
+// fs.writeFileSync('1-json.json', bookJSON)
+
+
+// console.log(data.title)
+
+//
+// Challenge: Work with JSON and the file system
+//
+// 1. Load and parse the JSON data
+// 2. Change the name and age property using your info
+// 3. Stringify the changed object and overwrite the original data
+// 4. Test your work by viewing data in the JSON file
+
+const dataBuffer = fs.readFileSync('1-json.json')
+const dataJSON = dataBuffer.toString()
+const user = JSON.parse(dataJSON)
+
+user.name = 'Patricia'
+user.age = 26
+
+const userJSON = JSON.stringify(user)
+fs.writeFileSync('1-json.json',userJSON)
