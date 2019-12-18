277a072271ba20f4873ae3c05bb569d0d47daa45
Patrícia Castro <patricia.castro@fabamaq.com>
2019-12-18 10:58:10 +0000

    Add geocoding request challenge

--- weather-app/app.js
+++ weather-app/app.js
@@ -8,9 +8,27 @@ const url = 'https://api.darksky.net/forecast/9a0bf19a7ea34b1fa05df9e14ff1709e/3
 // 2. Test your work
 
 request({ url: url, json: true }, (error, response) => {
-    const data = response.body.currently
-    console.log('It is currently ' + data.temperature + ' degrees out.')
-    console.log('There is a ' + data.precipProbability + '% chance of rain.')
+    const body = response.body
+
+    console.log(body.daily.data[0].summary)
+    console.log('It is currently ' + body.currently.temperature + ' degrees out.')
+    console.log('There is a ' + body.currently.precipProbability + '% chance of rain.')
 })
 
+// Geocoding
+// Address -> Lat/Long -> Weather
 
+// Goal: Print the lat/long for Los Angeles
+// 1. Fire off a new request to the URL explored in browser
+// 2. Have the request module parse it as JSON
+// 3.Print both the latitude and longitude to the terminal
+// 4. Test your work
+
+const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYXBhdHJpY2lhbWNhc3RybyIsImEiOiJjazRiNGxiOHcwYWRzM21wbTNraTJoaWE0In0.q6ELa9nSIHIuEfNG4tQxcw&limit=1'
+
+request({ url: geocodeURL , json: true }, (error, response) => {
+    const longitude = response.body.features[0].center[0]
+    const latitude = response.body.features[0].center[1]
+    console.log('Latitude: ' + latitude)
+    console.log('Longitude: ' + longitude)
+})
