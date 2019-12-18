b20167d39718a407569ab193de73ceb81ea17655
Patrícia Castro <patricia.castro@fabamaq.com>
2019-12-18 16:32:28 +0000

    Add Callback Chaining challenge

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,7 +1,7 @@
 const geocode = require ('./utils/geocode.js')
 const forecast = require('./utils/forecast.js')
 
-geocode('Porto,Portugal', (error, data) => {
+/* geocode('Porto,Portugal', (error, data) => {
     console.log('Error', error)
     console.log('Data', data)
 })
@@ -19,3 +19,34 @@ forecast(41.1524, -8.6351, (error, data) => {
     console.log('Error', error)
     console.log('Data', data)
   })
+ */
+
+
+// Goal: Accept location via command line argument
+//
+// 1. Access the location line argument without yargs
+// 2. Use the string value as the input for geocode
+// 3. Only geocode if a location was provided
+// 4. Test your work with a couple locations 
+const location = process.argv[2]
+
+if(!location) {
+    console.log('Please provide a location.')
+} else {
+    geocode(location, (error, data) => {
+        if(error) {
+           return console.log(error)
+        } else if(location) {
+            forecast(data.latitude, data.longitude, (error, forecastData) => {
+                if(error) {
+                    return console.log(error)
+                }
+                location === data.location
+                console.log(data.location)
+                console.log(forecastData)
+          })
+        }
+        
+    })
+}
+  
