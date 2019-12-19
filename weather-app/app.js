996a29eb104f711e23b5cba43e3f895ee7c1edb5
Patrícia Castro <patricia.castro@fabamaq.com>
2019-12-19 09:52:01 +0000

    Add destructuring and property shorthand challenge

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,48 +1,26 @@
 const geocode = require ('./utils/geocode.js')
 const forecast = require('./utils/forecast.js')
 
-/* geocode('Porto,Portugal', (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-})
+// Goal: Use both destructuring and property shorthand in weather app
+// 1. Use destructuring in app.js, forecast.js and geocode.js
+// 2. Use property shorthand in forecast.js and geocode.js
+// 3. Test your work and ensure app still works
 
-// Goal: Create a reusable function for getting the forecast
-//
-// 1. Setup the "forecast" function in utils/forecast.js
-// 2. Require the function in app.js and call it as shown below
-// 3. The forecast function should have three potential calls to callback:
-//    - Low level error, pass string for error
-//    - Coordinate error, pass string for error
-//    - Success, pass forecast string for data (same format as from before)
+const search = process.argv[2]
 
-forecast(41.1524, -8.6351, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-  })
- */
-
-
-// Goal: Accept location via command line argument
-//
-// 1. Access the location line argument without yargs
-// 2. Use the string value as the input for geocode
-// 3. Only geocode if a location was provided
-// 4. Test your work with a couple locations 
-const location = process.argv[2]
-
-if(!location) {
+if(!search) {
     console.log('Please provide a location.')
 } else {
-    geocode(location, (error, data) => {
+    geocode(search, (error, { latitude, longitude, location }) => {
         if(error) {
            return console.log(error)
         } else if(location) {
-            forecast(data.latitude, data.longitude, (error, forecastData) => {
+            forecast(latitude, longitude, (error, forecastData) => {
                 if(error) {
                     return console.log(error)
                 }
-                location === data.location
-                console.log(data.location)
+                search === location
+                console.log(location)
                 console.log(forecastData)
           })
         }
--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -3,17 +3,17 @@ const request = require ('request')
 const forecast = (latitude,longitude, callback) => {
     const url = 'https://api.darksky.net/forecast/9a0bf19a7ea34b1fa05df9e14ff1709e/'+ latitude + ',' + longitude +'?units=si'
 
-    request({ url: url, json: true }, (error, response) => {
+    request({ url, json: true }, (error, { body }) => {
         if(error){
             callback('Unable to connect to weather services.')
-        } else if(response.body.error) {
+        } else if(body.error) {
             callback('Unable to find location. Try again.')
         } else {
             callback(undefined,
-                response.body.daily.data[0].summary +
-                'It is currently ' + response.body.currently.temperature +
+                body.daily.data[0].summary +
+                'It is currently ' + body.currently.temperature +
                 ' degrees outside. There is a ' +
-                response.body.currently.precipProbability +
+                body.currently.precipProbability +
                 '% chance of rain.'           
             )
         }
