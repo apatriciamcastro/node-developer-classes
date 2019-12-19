87620b7a7bbd5ce5112fc83478ba22acdd7f3c53
Patrícia Castro <patricia.castro@fabamaq.com>
2019-12-19 15:41:30 +0000

    Add partials handlebars challenge

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,10 +1,26 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
+
+// Goal: Create a partial for the footer
+// 1. Setup the template for the footer partial "Created by..."
+// 2. Render the partial at the bottom of all three pages
+// 3. Test your work by visiting all three pages
 
 const app = express()
-app.set('view engine', 'hbs')
 
-app.use(express.static(path.join(__dirname, '../public')))
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
+app.use(express.static(path.join(publicDirectoryPath)))
 
 app.get('', (req, res) => {
     res.render('index', {
@@ -28,6 +44,7 @@ app.get('/about', (req, res) => {
 app.get('/help', (req, res) => {
     res.render('help', {
         title: 'Help Page',
+        name: 'Ana Castro',
         message: 'For help info, please contact system admin.'
     })
 })
