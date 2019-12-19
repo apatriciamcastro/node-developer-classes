9e0ea7d4cd57bac0c05ad0fef83cb90234d06979
Patrícia Castro <patricia.castro@fabamaq.com>
2019-12-19 11:19:38 +0000

    Add routes update challenge

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -30,14 +30,23 @@ app.get('/help', (req, res) => {
 // 2. Setup a weather route and render a page title
 // 3. Test your work by visiting both in the browser
 
+ // Goal: Update routes
+ // 1. Setup about route to render a title with HTML
+ // 2. Setup a weather route to send back JSON
+ //     - Object with forecast and location strings
+ // 3. Test your work by visiting both in the browser
+ 
 // app.com/about
 app.get('/about', (req, res) => {
-    res.send('About Page')
+    res.send('<h1>About Page</h1>')
 })
 
 // app.com/weather
 app.get('/weather', (req, res) => {
-    res.send('Weather Page')
+    res.send({
+        forecast: 'Rain',
+        location: 'Porto'
+    })
 })
 
 // To start up the server
