4edf28d889ea52accd31c4e074d324e3e7ba7195
Patrícia Castro <patricia.castro@fabamaq.com>
2019-12-20 11:24:31 +0000

    Add query string challenge

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -39,11 +39,35 @@ app.get('/help', (req, res) => {
     })
 })
 
+// Goal: Update weather endpoint to accept address
+// 1. No address? Send back an error message
+// 2. Address? Send back the static JSON
+//      - Add address property onto JSON which returns the provided address
+// 3. Test /weather and /weather?address=porto
+
 app.get('/weather', (req, res) => {
+    if(!req.query.address) {
+        return res.send({
+            errorMessage: 'You must provide an address.'
+        })
+    }
     res.send({
         forecast: 'Rain',
-        location: 'Porto'
+        location: 'Porto',
+        address: req.query.address
     })
+})
+
+app.get('/products', (req, res) => {
+    if(!req.query.search) {
+        return res.send({
+            errorMessage: 'You must provide a search term.'
+        })
+    }
+    console.log(req.query.search)
+    res.send({
+        products: []
+    })    
 })
 
 app.get('/help/*', (req, res) => {
