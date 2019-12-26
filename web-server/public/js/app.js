6dd9aef3cecafae1782d211adf4375a50ee73e36
Patrícia Castro <patricia.castro@fabamaq.com>
2019-12-26 10:47:32 +0000

    Add fetch HTTP request challenge

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,19 @@
-console.log('Client JS')
+console.log('Client side JS')
+
+// Goal: Fetch weather
+// 1. Setup a call to fetch weather for Porto, Portugal
+// 2. Get the parse JSON response
+//  - If error property, print error
+//  - If no error property, print location and forecast
+// 3. Refresh the browser and test your work
+
+fetch('http://localhost:3000/weather?address=porto,portugal').then((response) => {
+    response.json().then((data) => {
+        if(data.error){
+            console.log(data.error)
+        } else {
+            console.log(data.location)
+            console.log(data.forecast)
+        }
+    })
+})
