b110a2532d452676d2940058acc89494e2637cd1
Patrícia Castro <patricia.castro@fabamaq.com>
2019-12-27 17:07:23 +0000

    Add resource reading endpoints challenge

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -25,10 +25,26 @@ app.post('/users', (request, response) => {
     })
 })
 
-// Goal: Setup the task creation endpoint
-// 1. Create a separate file for the task model (load it into index.js)
-// 2. Create the task creation endpoint (handle success and error)
-// 3. Test the endpoint from postman with good and bad data
+app.get('/users', (request, response) => {
+    User.find({}).then((users) => {
+        response.send(users)
+    }).catch((error) => {
+        response.status(500).send()
+    })   
+})
+
+app.get('/users/:id', (request, response) => {
+    const _id = request.params.id
+
+    User.findById(_id).then((user) => {
+        if(!user) {
+            return response.status(404).send()
+        }
+        response.send(user)
+    }).catch((error) => {
+        response.status(500).send()
+    })   
+})
 
 app.post('/tasks', (request, response) => {
     const task = new Task(request.body)
@@ -37,6 +53,31 @@ app.post('/tasks', (request, response) => {
         response.status(201).send(task)
     }).catch((error) => {
         response.status(400).send(error)
+    })
+})
+
+// Goal: Setup the task reading endpoints
+// 1. Create an endpoint for fetching all tasks
+// 2. Create an endpoint for fetching a task by its id
+// 3. Setup new requests in Postman and test your work
+
+app.get('/tasks', (request, response) => {
+    Task.find({}).then((tasks) => {
+        response.send(tasks)
+    }).catch((error) => {
+        response.status(500).send()
+    })
+})
+
+app.get('/tasks/:id', (request, response) => {
+    const _id = request.params.id
+
+    Task.findById(_id).then((task) => {
+        if(!task) {
+            return response.status(404).send()
+        }
+        response.send(task)
+
     })
 })
 
