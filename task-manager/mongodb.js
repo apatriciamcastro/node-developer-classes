3e4ff8c1c54799107a6eda8e0368401fe63eb74f
Patrícia Castro <patricia.castro@fabamaq.com>
2019-12-27 11:25:59 +0000

    Add update challenge

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,8 +1,6 @@
-// CRUD: Create, Read, Update, Delete
+// CRUD: Create / Read / Update / Delete
+//       insert / find / update 
 
-// const mongodb = require('mongodb')
-// const MongoClient = mongodb.MongoClient
-// const ObjectID = mongodb.ObjectID
 const { MongoClient, ObjectID } = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
@@ -15,31 +13,35 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true } , (error, client) =
     
    const db = client.db(databaseName)
 
-//    db.collection('users').findOne({ _id: new ObjectID("5e05c90b50fe6626a7a7a3b5") }, (error, user) => {
-//        if(error) {
-//            return console.log('Unable to fetch user.')
-//        }
-//        console.log(user)
-//    })
+    // db.collection('users').updateOne({
+    //     _id: new ObjectID('5e05c90b50fe6626a7a7a3b6')
+    // }, {
+    //     $inc: {
+    //         age: 3
+    //     }
+    // }).then((result) => {
+    //     console.log(result)
+    // }).catch((error) => {
+    //     console.log(error)
+    // })
 
-//    db.collection('users').find({age:27}).toArray((error, users) => {
-//        console.log(users)
-//    })
-//    db.collection('users').find({age:27}).count((error, users) => {
-//     console.log(users)
-// })
+    // Goal: Use updateMany to complete all tasks
+    // 1. Check the documentation for updateMany
+    // 2. Setup the call with the query and the updates
+    // 3. Use promise methods to setup the success/error handlers
+    // 4. Test your work!
 
-// Goal: Use find and findOne with tasks
-// 1. Use findOne to fetch the last task by its id (print doc to console)
-// 2. Use find to fetch all tasks that are not completed (print docs to console)
-// 3. Test your work!
-
-    db.collection('tasks').findOne({ _id: new ObjectID("5e05caabe4ad2a2884173b37") }, (error, task) => {
-        console.log(task)
+    db.collection('tasks').updateMany({completed: false}, {
+        $set: {
+            completed: true
+        }
+    }).then((result) => {
+        console.log(result)
+    }).catch((error) => {
+        console.log(error)
     })
 
-    db.collection('tasks').find({ completed: false }).toArray((error, tasks) => {
-        console.log(tasks)
-    })
+
 
 })
+    
