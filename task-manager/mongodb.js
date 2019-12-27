f4be2a8f307d46856f9dc7037349f266062fac4a
Patrícia Castro <patricia.castro@fabamaq.com>
2019-12-27 11:36:32 +0000

    Add delete challenge

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,5 +1,5 @@
 // CRUD: Create / Read / Update / Delete
-//       insert / find / update 
+//       insert / find / update / delete
 
 const { MongoClient, ObjectID } = require('mongodb')
 
@@ -13,35 +13,27 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true } , (error, client) =
     
    const db = client.db(databaseName)
 
-    // db.collection('users').updateOne({
-    //     _id: new ObjectID('5e05c90b50fe6626a7a7a3b6')
-    // }, {
-    //     $inc: {
-    //         age: 3
-    //     }
-    // }).then((result) => {
-    //     console.log(result)
-    // }).catch((error) => {
-    //     console.log(error)
-    // })
-
-    // Goal: Use updateMany to complete all tasks
-    // 1. Check the documentation for updateMany
-    // 2. Setup the call with the query and the updates
-    // 3. Use promise methods to setup the success/error handlers
-    // 4. Test your work!
-
-    db.collection('tasks').updateMany({completed: false}, {
-        $set: {
-            completed: true
-        }
+    db.collection('users').deleteMany({
+        age: 27
     }).then((result) => {
         console.log(result)
     }).catch((error) => {
         console.log(error)
     })
 
+    // Goal: Use deleteOne to remove a task
+    // 1. Grab the description for the task you want to remove
+    // 2. Setup the call with the query
+    // 3. Use promise methods to setup the success/error handlers
+    // 4. Test your work
 
+    db.collection('tasks').deleteOne({
+        description: 'Wash dishes'
+    }).then((result) => {
+        console.log(result)
+    }).catch((error) => {
+        console.log(error)
+    })
 
 })
     
