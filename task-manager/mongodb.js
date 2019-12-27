d5ec76a24dd16db3cf19fd380ebdad365dedf628
Patrícia Castro <patricia.castro@fabamaq.com>
2019-12-27 09:21:11 +0000

    Add Insert Documents challenge

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -12,8 +12,63 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true } , (error, client) =
     }
     
    const db = client.db(databaseName)
-   db.collection('users').insertOne({
-       name: 'Patricia',
-       age: 27
-   })
+//    db.collection('users').insertOne({
+//        name: 'Patricia',
+//        age: 27
+//    }, (error, result) => {
+//        if(error) {
+//            return console.log('Unable to insert user.')
+//        }
+
+//        console.log(result.ops)
+
+//    })
+    // db.collection('users').insertMany([
+    //     {
+    //         name: 'Ana',
+    //         age: 28
+    //     },
+    //     {
+    //         name: 'Gunther',
+    //         age: 27
+    //     }
+    // ], (error, result) => {
+    //     if(error) {
+    //        return console.log('Unable to insert documents.')
+    //     }
+
+    //     console.log(result.ops)
+
+    // })
+
+
+    // Goal: Insert 3 tasks into a new tasks collection
+    // 1. Use insertMany to insert the documents
+    //      - description (string), completed (boolean)
+    // 2. Setup the callback to handle error or print ops
+    // 3. Run the script
+    // 4. Refresh the database in Robo 3T and view data in tasks collection
+
+    db.collection('tasks').insertMany([
+        {
+            description: 'Wash dishes',
+            completed: false
+        },
+        {
+            description: 'Vaccum floors',
+            completed: false
+        },
+        {
+            description: 'Cook dinner',
+            completed: true
+        }
+    ], (error, result) => {
+        if(error) {
+            return console.log('Unable to insert tasks.')
+        }
+
+        console.log(result.ops)
+
+    })
+
 })
