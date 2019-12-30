d4a7922941fcb01ea859d7385e17072fc7043a8f
Patrícia Castro <patricia.castro@fabamaq.com>
2019-12-30 11:40:24 +0000

    Add async/await challenge

--- task-manager/playground/promise-chaining-2.js
+++ task-manager/playground/promise-chaining-2.js
@@ -1,17 +1,32 @@
-// Goal: Mess around with promise chaining
-// 1. Create promise-chaining-2.js
-// 2. Load in mongoose and task model
-// 3. Remove a given task by id
-// 4. Get and print the total number of incomplete tasks
-// 5. Test your work
-
 require('../src/db/mongoose')
 const Task = require('../src/models/task')
 
-Task.findByIdAndDelete('5e05f41fbca2474da1247c60').then(() => {
+
+/* Task.findByIdAndDelete('5e05f41fbca2474da1247c60').then(() => {
     return Task.countDocuments( { completed: false })
 }).then((result) => {
     console.log(result)
+}).catch((error) => {
+    console.log(error)
+}) */
+
+// Goal: Use async/await
+// 1. Create deleteTaskAndCount as an async function
+//      - Accept id of task to remove
+// 2. Use await to delete task and count up incomplete tasks
+// 3. Return the count
+// 4. Call the function and attach then/catch to log results
+// 5. Test your work
+
+
+const deleteTaskAndCount = async (taskId) => {
+    const task = await Task.findByIdAndDelete(taskId)
+    const count = await Task.countDocuments( { completed: false })
+    return count
+}
+
+deleteTaskAndCount('5e05fd62f745e255bdc81155').then((count) => {
+    console.log(count)
 }).catch((error) => {
     console.log(error)
 })
