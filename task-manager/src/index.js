a1e8a9b14a066cff30772594c60d77c019b7ef9f
Patrícia Castro <patricia.castro@fabamaq.com>
2019-12-31 15:39:21 +0000

    Add timestamps challenge

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -25,19 +25,3 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is listening on port ' + port)
 })
-
-const Task = require('./models/task')
-const User = require('./models/user')
-
-const main = async () => {
-    // const task = await Task.findById('5e0b569e87be3c69cb388663')
-    // // to find the user who's associated with this task 
-    // await task.populate('owner').execPopulate()
-    // console.log(task.owner)
-
-    const user = await User.findById('5e0b556f8e1e9a68e5341564')
-    await user.populate('tasks').execPopulate()
-    console.log(user.tasks)
-}
-
-main()
--- task-manager/src/models/task.js
+++ task-manager/src/models/task.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose')
 
-const Task = mongoose.model('Task', {
+// Goal: Refactor task model to add timestamps
+// 1. Explicitly create schema
+// 2. Setup timestamps
+// 3. Create tasks from postman to test
+
+const taskSchema = new mongoose.Schema({
     description:{
         type: String,
         trim: true,
@@ -15,6 +20,10 @@ const Task = mongoose.model('Task', {
         required: true,
         ref: 'User'
     }
+},{
+    timestamps: true  
 })
+
+const Task = mongoose.model('Task', taskSchema)
 
 module.exports = Task
