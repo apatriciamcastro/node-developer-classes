1da769afbb0c003291931d2ff839cc9701adfaf5
Patrícia Castro <patricia.castro@fabamaq.com>
2020-01-02 15:19:17 +0000

    Add environment variables challenge

--- task-manager/src/emails/account.js
+++ task-manager/src/emails/account.js
@@ -1,8 +1,12 @@
 const sgMail = require('@sendgrid/mail')
 
-const sendgridAPIKey = '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'
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
-sgMail.setApiKey(sendgridAPIKey)
+// Goal: Pull JWT secret and database URL into env vars
+// 1. Create two new env vars: JWT_SECRET and MONGODB_URL
+// 2. Setup values for each in the development env files
+// 3. Swap out three hardcoded values
+// 4. Test your work. Create new user and get their profile
 
 const sendWelcomeEmail = (email, name) => {
     sgMail.send({
@@ -13,13 +17,6 @@ const sendWelcomeEmail = (email, name) => {
     })
     
 }
-
-// Goal: Send email to user on cancelation
-// 1. Setup a new function for sending an email on cancelation
-//      - email and name as args
-// 2. Include their name in the email and ask why they cancelled
-// 3. Call it just after the account is removed
-// 4. Run the request and check your inbox
 
 const sendCancellationEmail = (email, name) => {
     sgMail.send({
