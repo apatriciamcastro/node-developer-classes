447874a89a51177695ec6bcf3e232a4580f4b642
Patrícia Castro <patricia.castro@fabamaq.com>
2020-01-03 10:03:48 +0000

    Add asynchronous tests

--- task-manager/tests/math.test.js
+++ task-manager/tests/math.test.js
@@ -1,4 +1,4 @@
-const { calculateTip, fahrenheitToCelsius, celsiusToFahrenheit } = require('../src/math')
+const { calculateTip, fahrenheitToCelsius, celsiusToFahrenheit, add } = require('../src/math')
 
 test('Should calculate total with tip', () => {
     const total = calculateTip(10, .3)
@@ -10,13 +10,6 @@ test('Should calculate total with default tip', () => {
     expect(total).toBe(12.5)
 })
 
-// Goal: Test temperature conversion functions
-//
-// 1. Export both functions and load them into test suite
-// 2. Create "Should convert 32 F to 0 C"
-// 3. Create "Should convert 0 C to 32 F"
-// 4. Run the Jest to test your work!
-
 test('Should convert 32 F to 0 C', () => {
     const conversion = fahrenheitToCelsius(32)
     expect(conversion).toBe(0)
@@ -26,3 +19,23 @@ test('Should convert 0 C to 32 F', () => {
     const conversion = celsiusToFahrenheit(0)
     expect(conversion).toBe(32)
 })
+
+// test('Async test demo', (done) => {
+//     setTimeout(() => {
+//         expect(1).toBe(2)
+//         done()
+//     }, 2000)
+// })
+
+test('Should add two numbers', (done) => {
+    add(2,3).then((sum) => {
+        expect(sum).toBe(5)
+        done()
+    })    
+})
+
+test('Should add two numbers async/await', async () => {
+    const sum = await add(10, 22)
+    expect(sum).toBe(32)
+})
+
