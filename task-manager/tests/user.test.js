7bc854947f03df4fb443dd9458b3fdc9472e0788
Patrícia Castro <patricia.castro@fabamaq.com>
2020-01-06 15:17:12 +0000

    Add login failure test challenge

--- task-manager/tests/user.test.js
+++ task-manager/tests/user.test.js
@@ -1,10 +1,41 @@
 const request = require('supertest')
 const app = require('../src/app')
+const User = require('../src/models/user')
+
+const userOne = {
+    name: 'Mike',
+    email: 'mike@example.com',
+    password: '56what!!'
+}
+
+beforeEach(async () => {
+    await User.deleteMany()
+    await new User(userOne).save()
+})
 
 test('Should signup a new user', async() => {
     await request(app).post('/users').send({
         name: 'Patricia Castro',
-        email: 'pcastro@example.com',
+        email: 'apatriciamcastro@gmail.com',
         password: 'Test97!'
     }).expect(201)
+})
+
+test('Should login existing user', async () => {
+    await request(app).post('/users/login').send({
+        email: userOne.email,
+        password: userOne.password
+    }).expect(200)
+})
+
+// Goal: Test login failure
+// 1. Create "Should not login nonexistent user"
+// 2. Send off the request with bad credentials
+// 3. Expect the correct status response
+// 4. Test your work
+test('Should not login nonexistent user', async () => {
+    await request(app).post('/users/login').send({
+        email: 'mik@example.com',
+        password: userOne.password
+    }).expect(400)
 })
