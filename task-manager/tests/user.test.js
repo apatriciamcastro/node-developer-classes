b55f97e10d771679547bbe6a39c525e24526a888
Patrícia Castro <patricia.castro@fabamaq.com>
2020-01-06 16:44:26 +0000

    Add advanced assertions tests challenge

--- task-manager/tests/user.test.js
+++ task-manager/tests/user.test.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose')
 const app = require('../src/app')
 const User = require('../src/models/user')
 
-
 const userOneId = new mongoose.Types.ObjectId()
 const userOne = {
     _id: userOneId,
@@ -22,18 +21,41 @@ beforeEach(async () => {
 })
 
 test('Should signup a new user', async() => {
-    await request(app).post('/users').send({
+    const response = await request(app).post('/users').send({
         name: 'Patricia Castro',
-        email: 'apatriciamcastro@gmail.com',
+        email: 'apat@gmail.com',
         password: 'Test97!'
     }).expect(201)
+
+    // assert that the database was changed correctly
+    // (there is a new user with the same ID as the one we're getting back in the response body)
+    const user = await User.findById(response.body.user._id)
+    expect(user).not.toBeNull()
+
+    // assertions about the response
+    expect(response.body).toMatchObject({
+        user: {
+            name: 'Patricia Castro',
+            email: 'apat@gmail.com'
+        },
+        token: user.tokens[0].token
+    })
+    expect(user.password).not.toBe('Test97!')
 })
 
+// Goal: Validate new token is saved
+// 1. Fetch the user from the database
+// 2. Assert that token in response matches users second token
+// 3. Test your work
+
 test('Should login existing user', async () => {
-    await request(app).post('/users/login').send({
+    const response = await request(app).post('/users/login').send({
         email: userOne.email,
         password: userOne.password
     }).expect(200)
+    const user = await User.findById(userOneId)
+
+    expect(response.body.token).toBe(user.tokens[1].token)
 })
 
 test('Should not login nonexistent user', async () => {
@@ -58,11 +80,9 @@ test('Should not get profile for unauthenticated user', async () => {
         .expect(401)
 })
 
-// Goal: Test delete account
-// 1. Create "Should delete account for user"
-//      - Setup auth header and expect correct status code
-// 2. Create "Should not delete account for unauthenticated user"
-//      - Expect correct status code
+// Goal: Validate user is removed
+// 1. Fetch the user from the database
+// 2. Assert null response (use assertion from signup test)
 // 3. Test your work
 
 test('Should delete account for user', async () => {
@@ -70,7 +90,11 @@ test('Should delete account for user', async () => {
         .delete('/users/me')
         .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
         .send()
-        .expect(200)       
+        .expect(200) 
+    const user = await User.findById(userOneId)
+    
+    expect(user).toBeNull()
+
 })
 
 test('Should not delete account for unauthenticated user', async () => {
