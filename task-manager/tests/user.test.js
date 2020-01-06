03fb38cd22927a0bc817cca5a6c99a93c900ddd7
Patrícia Castro <patricia.castro@fabamaq.com>
2020-01-06 15:45:56 +0000

    Add delete account tests challenge

--- task-manager/tests/user.test.js
+++ task-manager/tests/user.test.js
@@ -1,11 +1,19 @@
 const request = require('supertest')
+const jwt = require('jsonwebtoken')
+const mongoose = require('mongoose')
 const app = require('../src/app')
 const User = require('../src/models/user')
 
+
+const userOneId = new mongoose.Types.ObjectId()
 const userOne = {
+    _id: userOneId,
     name: 'Mike',
     email: 'mike@example.com',
-    password: '56what!!'
+    password: '56what!!',
+    tokens: [{
+        token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET)
+    }]
 }
 
 beforeEach(async () => {
@@ -28,14 +36,46 @@ test('Should login existing user', async () => {
     }).expect(200)
 })
 
-// Goal: Test login failure
-// 1. Create "Should not login nonexistent user"
-// 2. Send off the request with bad credentials
-// 3. Expect the correct status response
-// 4. Test your work
 test('Should not login nonexistent user', async () => {
     await request(app).post('/users/login').send({
         email: 'mik@example.com',
         password: userOne.password
     }).expect(400)
+})
+
+test('Should get profile for user', async () => {
+    await request(app)
+        .get('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200)
+})
+
+test('Should not get profile for unauthenticated user', async () => {
+    await request(app)
+        .get('/users/me')
+        .send()
+        .expect(401)
+})
+
+// Goal: Test delete account
+// 1. Create "Should delete account for user"
+//      - Setup auth header and expect correct status code
+// 2. Create "Should not delete account for unauthenticated user"
+//      - Expect correct status code
+// 3. Test your work
+
+test('Should delete account for user', async () => {
+    await request(app)
+        .delete('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200)       
+})
+
+test('Should not delete account for unauthenticated user', async () => {
+    await request(app)
+        .delete('/users/me')
+        .send()
+        .expect(401)  
 })
