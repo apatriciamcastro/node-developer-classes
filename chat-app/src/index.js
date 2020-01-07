6f6b5cc19e713e83a6fa1106a5de22bb4cc80152
Patrícia Castro <patricia.castro@fabamaq.com>
2020-01-07 14:26:22 +0000

    Add websocket setup

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -1,19 +1,21 @@
 const express = require('express')
+const http = require('http')
 const path = require('path')
+const socketio = require('socket.io')
 
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
 
 const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-app.listen(port, () => {
+io.on('connection', () => {
+    console.log('New websocket connection')
+} )
+
+server.listen(port, () => {
     console.log(`Server is listening on port ${port}`)
 })
-
-// Goal: Setup scripts in package.json
-// 1. Create a "start" script to start the app using node
-// 2. Install nodemon as a development dependency
-// 3. Create a "dev" script to start the app using nodemon
-// 4. Run both scripts to test your work
