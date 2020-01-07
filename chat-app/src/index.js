cf3f5fdbc93f9546e782880718a0d34cc4671c28
Patrícia Castro <patricia.castro@fabamaq.com>
2020-01-07 15:36:14 +0000

    Add socket.io events challenge

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -12,8 +12,23 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-io.on('connection', () => {
+
+// Goal: Allow clients to send messages
+// 1. Create a form with an input and button
+//      - Similar to the weather form
+// 2. Setup event listener for form submissions
+//      - Emit "sendMessage" with input string as message data
+// 3. Have server listen for "sendMessage"
+//      - Send message to all connected clients
+// 4. Test your work
+
+io.on('connection', (socket) => {
     console.log('New websocket connection')
+
+    socket.on('sendMessage', (message) => {
+        io.emit('message', message)
+    })
+
 } )
 
 server.listen(port, () => {
