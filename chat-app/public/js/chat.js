eb652b0ca2f3e083521dcfa8dd8b26852a19f8d4
Patrícia Castro <patricia.castro@fabamaq.com>
2020-01-08 09:18:59 +0000

    Add event acknowledgements challenge

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -8,27 +8,34 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
     e.preventDefault()
 
     const message = e.target.elements.message.value
-    socket.emit('sendMessage', message)
+
+    socket.emit('sendMessage', message, (error) => {
+        if(error) {
+            return console.log(error)
+        }
+        console.log('Message delivered!')
+       
+    })
 })
 
-// Goal: Share coordinates with other users
-// 1. Have client emit "sendLocation" with an object as the data
-//      - Object should contain latitude and longitude properties
-// 2. Server should listen for "sendLocation"
-//      - When fired, send a "message" to all connected clients "Location: long, lat"
-// 3. Test your work! :D
+// Goal: Setup acknowledgement for location
+// 1. Setup the client acknowledgment function
+// 2. Setup the server to send back the acknowledgement
+// 3. Have the client print "Location shared!" when acknowledged
+// 4. Test your work
 
 document.querySelector('#send-location').addEventListener('click', () => {
     if(!navigator.geolocation) {
         return alert('Geolocation not supported')
     }
+
     navigator.geolocation.getCurrentPosition((position) => {
         socket.emit('sendLocation', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
+    },() => {
+        console.log('Location shared!')  
         })
     })
 })
-
-
 
