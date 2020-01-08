47db2c40bf62ea47e6ba708f7684ec423135ddd7
Patrícia Castro <patricia.castro@fabamaq.com>
2020-01-08 10:59:48 +0000

    Add second rendering location messages challenge

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -9,6 +9,7 @@ const $messages = document.querySelector('#messages')
 
 // Templates
 const messageTemplate = document.querySelector('#message-template').innerHTML
+const locationTemplate = document.querySelector('#location-template').innerHTML
 
 
 socket.on('message', (message) => {
@@ -20,13 +21,21 @@ socket.on('message', (message) => {
     
 })
 
-// Goal: Create a separate event for location sharing messages
-// 1. Have server emit "locationMessage" with the URL
-// 2. Have the client listen for "locationMessage" and print the URL to the console
-// 3. Test your work by sharing a location
+// Goal: Render new template for location messages
+// 1. Duplicate the message template
+//      - Change the id to something else
+// 2. Add a link inside the paragraph with the link text "My current location"
+//      - URL for link should be the maps URL (dynamic)
+// 3. Select the template from JavaScript
+// 4. Render the template with the URL and append to messages list
+// 5. Test your work
 
 socket.on('locationMessage', (location) => {
     console.log(location)
+    const html = Mustache.render(locationTemplate, {
+        location
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
 })
 
 $messageForm.addEventListener('submit', (e) => {
